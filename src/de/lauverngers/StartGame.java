b6f6ec1ae4bdbc31e14d836f48eb8398e31ed400
Maren Kühle <m.kuehle@laudert.de>
2014-09-18 15:03:08 +0200

    added some rules for the game

---
+++ src/de/lauverngers/StartGame.java
@@ -0,0 +1,13 @@
+package de.lauverngers;
+
+/**
+ * Created by Maren on 18.09.14.
+ */
+public class StartGame {
+
+    public static void main(String[] args) {
+//        initGame
+//        startGame
+//        endGame
+    }
+}
